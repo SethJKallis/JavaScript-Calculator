c837e072547a241586fe0e01d3e180ab57aa753a
SethJKallis <kallisseth@gmail.com>
2022-11-21 13:34:49 +0200

    final calculator for JavaScript Project

---
+++ js/index.js
@@ -0,0 +1,59 @@
+let display = document.getElementById(`display-el`);
+display.innerText = " ";
+
+let numbers = [
+  document.getElementById(`num1`),
+  document.getElementById(`num2`),
+  document.getElementById(`num3`),
+  document.getElementById(`num4`),
+  document.getElementById(`num5`),
+  document.getElementById(`num6`),
+  document.getElementById(`num7`),
+  document.getElementById(`num8`),
+  document.getElementById(`num9`),
+  document.getElementById(`num0`),
+  document.getElementById(`decimal-el`),
+  document.getElementById(`plus-el`),
+  document.getElementById(`minus-el`),
+  document.getElementById(`multiply-el`),
+  document.getElementById(`divide-el`),
+  document.getElementById(`openParenthesis`),
+  document.getElementById(`closedParenthesis`),
+  document.getElementById(`equal-el`),
+  document.getElementById(`del-el`)
+]
+
+numbers.map( numberClicked => {
+    numberClicked.addEventListener(`click`, (e) => {
+        // console.log(`clicked`);
+        // console.log(e);
+        // console.log(e.target);
+        // console.log(e.target.innerText);
+        switch(e.target.innerText){
+            case `DEL`:
+                if(display.innerText){
+                    display.innerText = display.innerText.slice(0,-1)
+                }
+                break;
+            case `=`:
+                try{
+                    if(display.innerText == ""){
+                        display.innerText = ""
+                    }
+                   else{
+                    display.innerText = eval(display.innerText)
+                } 
+                }
+                catch{
+                    display.innerText = ''
+                }
+                break;
+            default:
+                 display.innerText += e.target.innerText
+        }
+    });
+})
+
+function clearAll(){
+    display.innerHTML = " "
+}
