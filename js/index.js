a58d651fedf2bebabcb7494a88c55bdb27517bfc
SethJKallis <kallisseth@gmail.com>
2022-11-24 16:15:54 +0200

    fixed the repeating decimal issue

--- js/index.js
+++ js/index.js
@@ -1,51 +1,78 @@
 let display = document.getElementById(`display-el`)
 let numbers = [
-  document.getElementById(`num1`),
-  document.getElementById(`num2`),
-  document.getElementById(`num3`),
-  document.getElementById(`num4`),
-  document.getElementById(`num5`),
-  document.getElementById(`num6`),
-  document.getElementById(`num7`),
-  document.getElementById(`num8`),
-  document.getElementById(`num9`),
-  document.getElementById(`num0`),
-  document.getElementById(`decimal-el`),
-  document.getElementById(`plus-el`),
-  document.getElementById(`minus-el`),
-  document.getElementById(`multiply-el`),
-  document.getElementById(`divide-el`),
-  document.getElementById(`openParenthesis`),
-  document.getElementById(`closedParenthesis`),
-  document.getElementById(`equal-el`),
-  document.getElementById(`del-el`)
+    document.getElementById(`num1`),
+    document.getElementById(`num2`),
+    document.getElementById(`num3`),
+    document.getElementById(`num4`),
+    document.getElementById(`num5`),
+    document.getElementById(`num6`),
+    document.getElementById(`num7`),
+    document.getElementById(`num8`),
+    document.getElementById(`num9`),
+    document.getElementById(`num0`),
+    document.getElementById(`decimal-el`),
+    document.getElementById(`plus-el`),
+    document.getElementById(`minus-el`),
+    document.getElementById(`multiply-el`),
+    document.getElementById(`divide-el`),
+    document.getElementById(`openParenthesis`),
+    document.getElementById(`closedParenthesis`),
+    document.getElementById(`equal-el`),
+    document.getElementById(`del-el`)
 ]
-numbers.map( numberClicked => {
+let numChecker = []
+numbers.map(numberClicked => {
     numberClicked.addEventListener(`click`, (e) => {
-        switch(e.target.innerText){
+        switch (e.target.innerText) {
             case `DEL`:
-                if(display.innerText){
-                    display.innerText = display.innerText.slice(0,-1)
+                console.log(e.target.innerText)
+                if (display.innerText) {
+                    numChecker.pop()
+                    display.innerText = display.innerText.slice(0, -1)
                 }
                 break;
             case `=`:
-                try{
-                    if(display.innerText == ""){
+                try {
+                    if (display.innerText == "") {
                         display.innerText = ""
                     }
-                   else{
-                    display.innerText = eval(display.innerText)
-                } 
+                    else {
+                        display.innerText = eval(display.innerText)
+                        numChecker = [];
+                        numChecker.push(display.innerText.split(''))
+                        console.log(numChecker)
+                    }
                 }
-                catch{
+                catch {
                     display.innerText = 'Err.'
                 }
                 break;
+            case `.`:
+                if (numChecker.includes(`.`)) {
+                    display.innerText = display.innerText;
+                }
+                else {
+                    
+                    display.innerText += e.target.innerText
+                    numChecker.push('.')
+                }
+                break;
+            case `*`:
+            case `/`:
+            case `-`:
+            case `+`:
+                if (display.innerText) {
+                    display.innerText += e.target.innerText
+                    numChecker = []
+                }
+                break;
             default:
-                 display.innerText += e.target.innerText
+                display.innerText += e.target.innerText
+                numChecker.push(e.target.innerText)
         }
     })
 })
-function clearAll(){
-    display.innerHTML = " "
+function clearAll() {
+    display.innerHTML = "";
+    numChecker = []
 }
